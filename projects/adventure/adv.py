3fc87b97d784e85ec18c9736fbb6154cd08a887b
Jasmine Terry <jasmine-terry91@gmail.com>
2020-06-12 12:59:32 -0400

    comments moved, code cleanup

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,15 +8,6 @@ from ast import literal_eval
 # Load world
 world = World()
 
-#dft(stack)
-#need to be able to go back!
-#keep track of opposite direction entered(go back)
-#keep track of path(visited exits)
-#keep track of room in
-#keep track of directions(exits) rooms visited(for going back)---room has [get_exits] & [get_room_in_directions] fn
-#player has [travel] fn to move
-
-
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
@@ -27,17 +18,45 @@ map_file = "maps/main_maze.txt"
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
-
 # Print an ASCII map
 #world.print_rooms()
+
+####################PLanning####################
+
+#dft(stack)
+#need to be able to go back!
+#keep track of opposite direction entered(go back)
+#keep track of path(visited exits)
+#keep track of room in
+#keep track of directions(exits) rooms visited(for going back)---room has [get_exits] & [get_room_in_directions] fn
+#player has [travel] fn to move
+
+#TODO--Traversal code(done)
+#keep track of visited rooms(set)
+#keep path in stack
+#rooms len for all rooms checked
+#while rooms not visited:
+#check exits in room
+#keep track of moves
+#keep track of exits for room
+#if room move direction hasnt been visited, visit(Add to visited) --add exits to moves
+#add room to visited
+#check for directions
+#get ran direction from omves avail
+#visit room
+#add to paths
+#if no exits: go back!
+#to go back:
+#no exits in room
+#go opposite of last move
+#add path
+#get last move, travel that way
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
-#TODO--Traversal code
 
 
 #use opposite of last direction entered to go back
@@ -51,44 +70,27 @@ def opposite_way(direction):
     elif direction == 'e':
         return 'w'
 
-
-#keep track of visited rooms(set)
 visited = set()
-#keep path in stack
 paths= Stack()
 
-#rooms len for all rooms checked
-#while rooms not visited:
 while len(visited) < len(world.rooms):
-      #check exits in room
+
     exits = player.current_room.get_exits()
     moves = []
-    #keep track of moves
-#keep track of exits for room
-#if room move direction hasnt been visited, visit(Add to visited) --add exits to moves
+
     for exit in exits:
         if exit is not None and player.current_room.get_room_in_direction(exit) not in visited:
             moves.append(exit)
-#add room to visited
     visited.add(player.current_room)
    
-   #check for directions
-     #get ran direction from omves avail
     if len(moves) >0:
         random_dir = random.randint(0, len(moves) -1)
-  #visit room
         player.travel(moves[random_dir])
-         #add to paths
         paths.push(moves[random_dir])
         traversal_path.append(moves[random_dir])
-    #if no exits: go back!
-#to go back:
-#no exits in room
-#go opposite of last move
-#add path?
+
     else:
         go_back = paths.pop()
-        #get last move, travel that way
         player.travel(opposite_way(go_back))
         traversal_path.append(opposite_way(go_back))
 
